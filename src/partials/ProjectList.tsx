f3ba6177c655cf1d7d1c825f8ac4c9b5e9972b87
Dorota Leniec <dorota.leniec.dev@gmail.com>
2023-11-22 20:46:33 +0100

    in progress project

--- src/partials/ProjectList.tsx
+++ src/partials/ProjectList.tsx
@@ -33,22 +33,29 @@ const ProjectList = () => (
         }
       />
 
-      <Project
-        name="Ecommerce Platform"
-        description="Store page intgrated with Ecommerce Admin Panel."
-        link="https://e-commerce-admin-4vw73nzme-dorotaleniecdev.vercel.app/"
-        img={{
-          src: "/assets/images/ecommerce-platform.png",
-          alt: "Project Web Design",
-        }}
-        category={
-          <>
-            <Tags color={ColorTags.BLUE}>NextJS</Tags>
-            <Tags color={ColorTags.GREEN}>Tailwind.css</Tags>
-            <Tags color={ColorTags.YELLOW}>TypeScript</Tags>
-          </>
-        }
-      />
+      <div className="relative">
+        <div className="absolute left-0 right-0 w-[100px] mx-auto top-0.5">
+          In progress...
+        </div>
+        <div className="opacity-10">
+          <Project
+            name="Ecommerce Platform"
+            description="Store page intgrated with Ecommerce Admin Panel."
+            link="https://e-commerce-admin-4vw73nzme-dorotaleniecdev.vercel.app/"
+            img={{
+              src: "/assets/images/ecommerce-platform.png",
+              alt: "Project Web Design",
+            }}
+            category={
+              <>
+                <Tags color={ColorTags.BLUE}>NextJS</Tags>
+                <Tags color={ColorTags.GREEN}>Tailwind.css</Tags>
+                <Tags color={ColorTags.YELLOW}>TypeScript</Tags>
+              </>
+            }
+          />
+        </div>
+      </div>
     </div>
   </Section>
 );
